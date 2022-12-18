74d36a0daeed59bb14da32f0e05446b3813706c2
Paul-Emmanuel Raoul <paul@skyplabs.com>
2022-12-18 17:01:52 +0100

    Add main logic

--- local.tf
+++ local.tf
@@ -1,3 +1,14 @@
 # https://www.terraform.io/docs/configuration/locals.html.
 locals {
+  # Map of records per target.
+  records = flatten([
+    for record in var.records : [
+      for target in record.targets : {
+        name   = record.name
+        type   = record.type
+        ttl    = record.ttl
+        target = target
+      }
+    ]
+  ])
 }
