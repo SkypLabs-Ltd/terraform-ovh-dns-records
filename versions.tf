0bbe54ba9acbd9b26864b2af33deecfe1221801f
Paul-Emmanuel Raoul <paul@skyplabs.com>
2022-12-18 16:56:27 +0100

    Set up OVH provider

--- versions.tf
+++ versions.tf
@@ -3,5 +3,10 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
+    # https://registry.terraform.io/providers/ovh/ovh.
+    ovh = {
+      source  = "ovh/ovh"
+      version = "~> 0.25"
+    }
   }
 }
